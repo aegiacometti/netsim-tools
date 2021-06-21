07ee59025e8bf58347d658662000b7c99333a842
Ivan Pepelnjak <ip@ipspace.net>
2021-06-21 07:54:53 +0200

    Initial CLI dispatcher and usage guidelines

--- netsim/cli/__init__.py
+++ netsim/cli/__init__.py
@@ -3,9 +3,24 @@
 # Main CLI entry point
 #
 
+import sys
+import importlib
+
 from .. import main
 from . import parser
+from . import usage
 
 def create_topology() -> None:
   args = parser.create_topology_parse()
   main(args)
+
+def lab_commands() -> None:
+  if len(sys.argv) < 2:
+    usage.run([])
+
+  cmd = sys.argv[1]
+  mod = importlib.import_module("."+cmd,__name__)
+  if not mod:
+    print("Invalid CLI command: %s. Use 'help' to get the list of valid commands")
+    sys.exit(1)
+  mod.run(sys.argv[2:])
