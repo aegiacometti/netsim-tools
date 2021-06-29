96b75f1c7cb56abcb9cd934cea06c1701feb9f17
Ivan Pepelnjak <ip@ipspace.net>
2021-06-29 12:29:44 +0200

    Make old-style topology transform work from create-topology and netlab

--- netsim/cli/topology.py
+++ netsim/cli/topology.py
@@ -4,12 +4,13 @@
 # Contain parsers for create-topology script and the main netlab CLI
 #
 import argparse
+import typing
 from .. import main
 
 #
 # CLI parser for create-topology script
 #
-def create_topology_parse() -> argparse.Namespace:
+def create_topology_parse(args: typing.Optional[typing.List[str]] = None) -> argparse.Namespace:
   parser = argparse.ArgumentParser(description='Create topology data from topology description')
   parser.add_argument('-t','--topology', dest='topology', action='store', default='topology.yml',
                   help='Topology file')
@@ -32,11 +33,10 @@ def create_topology_parse() -> argparse.Namespace:
                   help='Report only major errors')
   parser.add_argument('-v','--view', dest='verbose', action='store_true',
                   help='Display data instead of creating a file')
-  args = parser.parse_args()
+  return parser.parse_args(args)
 
-  return args
+def legacy() -> None:
+  main(create_topology_parse(None))
 
-def run() -> None:
-  args = create_topology_parse()
-  main(args)
-
+def run(cli_args: typing.List[str]) -> None:
+  main(create_topology_parse(cli_args))
