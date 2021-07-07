6ad00f7dbd5c6f26f6a7e7fc4a77c60e00566753
Ivan Pepelnjak <ip@ipspace.net>
2021-07-07 12:08:37 +0200

    netlab config: allow generic template arguments

--- netsim/cli/config.py
+++ netsim/cli/config.py
@@ -5,24 +5,29 @@
 #
 import typing
 import argparse
+import os
+import glob
 
 from . import common_parse_args
 from . import ansible
+from .. import common
 
 #
 # CLI parser for 'netlab config' command
 #
 def custom_config_parse(args: typing.List[str]) -> typing.Tuple[argparse.Namespace, typing.List[str]]:
   parser = argparse.ArgumentParser(
-    parents=[ common_parse_args() ],
-    prog="netlab config",
+    prog='netlab config',
     description='Deploy custom configuration template',
     epilog='All other arguments are passed directly to ansible-playbook')
-
+  parser.add_argument(
+    '-v','--verbose',
+    dest='verbose',
+    action='store_true',
+    help='Verbose logging')
   parser.add_argument(
     dest='template', action='store',
-    type=argparse.FileType('r'),
-    help='Deploy just the initial configuration')
+    help='Configuration template (or a family of templates)')
 
   return parser.parse_known_args(args)
 
@@ -32,9 +37,14 @@ def run(cli_args: typing.List[str]) -> None:
   if args.verbose:
     rest = ['-v'] + rest
 
-  rest = ['-e','config='+args.template.name] + rest
+  if args.template != '-':
+    if os.path.exists(args.template) or \
+       os.path.exists(args.template+'.j2') or \
+       glob.glob(args.template+'.*.j2'):
+      rest = ['-e','config='+args.template] + rest
+    else:
+      common.fatal('Cannot find specified Jinja2 template: %s' % args.template,'config')
 
-  if args.logging or args.verbose:
-    print("Ansible playbook args: %s" % rest)
-
+  if args.verbose:
+    print('Ansible playbook args: %s' % rest)
   ansible.playbook('config.ansible',rest)
