21fa926626ff59c324a88d35016872e820eb94d9
Ivan Pepelnjak <ip@ipspace.net>
2021-07-17 12:27:34 +0200

    Code comments

--- netsim/common.py
+++ netsim/common.py
@@ -51,13 +51,6 @@ def template(j2: str , data: typing.Dict, path: str) -> str:
   template = ENV.get_template(j2)
   return template.render(**data)
 
-def null_to_string(d: typing.Dict) -> None:
-  for k in d.keys():
-    if isinstance(d[k],dict):
-      null_to_string(d[k])
-    elif d[k] is None:
-      d[k] = ""
-
 def set_verbose() -> None:
   global VERBOSE
   VERBOSE=True
@@ -66,6 +59,20 @@ def print_verbose(t: typing.Any) -> None:
   if VERBOSE:
     print(t)
 
+#
+# Change all NULL values in a nested dictionary structure to empty strings
+# to make them nicer in YAML printouts
+#
+def null_to_string(d: typing.Dict) -> None:
+  for k in d.keys():
+    if isinstance(d[k],dict):
+      null_to_string(d[k])
+    elif d[k] is None:
+      d[k] = ""
+
+#
+# Set a dictionary value specified by a list of keys
+#
 def set_dots(b : dict,k_list : list,v : typing.Any) -> None:
   if len(k_list) <= 1:
     b[k_list[0]] = v
@@ -74,6 +81,9 @@ def set_dots(b : dict,k_list : list,v : typing.Any) -> None:
     b[k_list[0]] = {}
   set_dots(b[k_list[0]],k_list[1:],v)
 
+#
+# Change dotted dictionary keys into nested dictionaries
+#
 def unroll_dots(b : typing.Any) -> None:
   if isinstance(b,dict):
     for k in list(b.keys()):
